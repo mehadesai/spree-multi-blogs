21dd8371c145ba89b98109a80438b4b9df138328
Meha Desai <meha@urbanladder.com>
2015-09-16 18:14:19 +0530

    changing permalink of blog post to blog/:slug

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Spree::Core::Engine.routes.draw do
 
   resources :blog_entries
 
-  get '/blog/tag/:tag' => 'blog_entries#tag', :as => :blog_tag
+#  get '/blog/tag/:tag' => 'blog_entries#tag', :as => :blog_tag
   get '/blog/category/:category' => 'blog_entries#category', :as => :blog_category
   get '/blog/author/:author' => 'blog_entries#author', :as => :blog_author
-  get '/blog/:year/:month/:day/:slug' => 'blog_entries#show', :as => :blog_entry_permalink
+  get '/blog/:slug' => 'blog_entries#show', :as => :blog_entry_permalink
   get '/blog/:year(/:month)(/:day)' => 'blog_entries#archive', :as => :news_archive, 
     :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
   get '/blog/feed' => 'blog_entries#feed', :as => :blog_feed, :format => :rss
