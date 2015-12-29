ed2860c3cda8708d481b11684b8b5387d91c00d7
Meha Desai <meha@urbanladder.com>
2015-12-29 12:25:14 +0530

    controller changes for blogs and blog posts

--- app/controllers/spree/admin/blog_entries_controller.rb
+++ app/controllers/spree/admin/blog_entries_controller.rb
@@ -1,15 +1,58 @@
-class Spree::Admin::BlogEntriesController < Spree::Admin::ResourceController
-  helper 'spree/blog_entries'
+module Spree
+  module Admin
+    class BlogEntriesController < ResourceController
+      PER_PAGE = 30
+      belongs_to 'spree/blogs'
+      load_and_authorize_resource :blog
+      load_and_authorize_resource :blog_entry, through: :blog
 
-  private
-    def location_after_save
-      edit_admin_blog_url(@blog_entry.blog)
+      def index
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= "1"
+
+        @blog = Spree::Blog.find(params[:blog_id])
+        @blog_entries = @blog_entries.unscoped if params[:q].delete(:deleted_at_null) == '0'
+        @search = @blog_entries.ransack(params[:q])
+        @blog_entries = @search.result.page(params[:page]).per(PER_PAGE)
+      end
+
+      def update
+        if params[:blog_entry].present? && params[:blog_entry][:deleted_at].present?
+          params[:blog_entry][:deleted_at] = params[:blog_entry][:deleted_at] == '0' ? nil : Time.now
+        end
+        @blog = Spree::Blog.find(params[:blog_id])
+        result = @blog_entry.update_attributes(blog_entry_params)
+        if result
+          flash[:success] = flash_message_for(@blog_entry, :successfully_updated)
+          respond_with(@blog_entry) do |format|
+            format.html { redirect_to admin_blog_blog_entries_path }
+            format.json { render layout: false, status: :updated }
+          end
+        else
+          invoke_callbacks(:update, :fails)
+          respond_with(@blog_entry)
+        end
+      end
+
+      private
+        def location_after_save
+          edit_admin_blog_url(@blog_entry.blog)
+        end
+
+        def collection
+          page = params[:page].to_i > 0 ? params[:page].to_i : 1
+          per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 20
+          Spree::BlogEntry.where(blog_id: params[:blog_id]).page(page).per(per_page)
+        end
+
+      private
+        def load_data
+          @blog = Spree::Blog.find(params[:blog_id])
+        end
+
+        def blog_entry_params
+          params.require(:blog_entry).permit(:title, :visible, :blog_id, :body, :permalink, :author_id, :summary, :published_at)
+        end
     end
-
-    def collection
-      page = params[:page].to_i > 0 ? params[:page].to_i : 1
-      per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 20
-      model_class.page(page).per(per_page)
-    end
-
+  end
 end
--- app/controllers/spree/admin/blogs_controller.rb
+++ app/controllers/spree/admin/blogs_controller.rb
@@ -1,45 +1,49 @@
-class Spree::Admin::BlogsController < Spree::Admin::ResourceController
-  def index
-    params[:q] ||= {}
-    params[:q][:deleted_at_null] ||= "1"
+module Spree
+  module Admin
+    class BlogsController < ResourceController
+      def index
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= "1"
 
-    @blogs = @blogs.unscoped if params[:q].delete(:deleted_at_null) == '0'
-    @search = @blogs.ransack(params[:q])
-    @logs = @search.result.page(params[:page]).per(30)
-  end
-
-  def update
-    if params[:blog].present? && params[:blog][:deleted_at].present?
-      params[:blog][:deleted_at] = params[:blog][:deleted_at] == '0' ? nil : Time.now
-    end
-    result = @blog.update_attributes(blog_params)
-    if result
-      flash[:success] = flash_message_for(@blog, :successfully_updated)
-      respond_with(@blog) do |format|
-        format.html { redirect_to admin_blogs_path }
-        format.json { render layout: false, status: :updated }
+        @blogs = @blogs.unscoped if params[:q].delete(:deleted_at_null) == '0'
+        @search = @blogs.ransack(params[:q])
+        @blogs = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
       end
-    else
-      invoke_callbacks(:update, :fails)
-      respond_with(@blog)
+
+      def update
+        if params[:blog].present? && params[:blog][:deleted_at].present?
+          params[:blog][:deleted_at] = params[:blog][:deleted_at] == '0' ? nil : Time.now
+        end
+        result = @blog.update_attributes(blog_params)
+        if result
+          flash[:success] = flash_message_for(@blog, :successfully_updated)
+          respond_with(@blog) do |format|
+            format.html { redirect_to admin_blogs_path }
+            format.json { render layout: false, status: :updated }
+          end
+        else
+          invoke_callbacks(:update, :fails)
+          respond_with(@blog)
+        end
+      end
+
+      protected
+        def location_after_save
+          edit_admin_blog_url(@blog)
+        end
+
+        def collection
+          page = params[:page].to_i > 0 ? params[:page].to_i : 1
+          per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 20
+          Spree::Blog.page(page).per(per_page)
+        end
+
+
+      private
+        def blog_params
+          params.require(:blog).permit(:name, :enabled, :private, :slug, :css_path, :deleted_at)
+        end
     end
   end
-
-  protected
-    def location_after_save
-      edit_admin_blog_url(@blog)
-    end
-
-    def collection
-      page = params[:page].to_i > 0 ? params[:page].to_i : 1
-      per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 20
-      model_class.page(page).per(per_page)
-    end
-
-
-  private
-    def blog_params
-      params.require(:bundle).permit(:name, :enabled, :deleted_at)
-    end
 end
 
