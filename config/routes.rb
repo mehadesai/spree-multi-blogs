a921ffb2a0f99371cc95891908343f4e82460377
Meha Desai <meha@urbanladder.com>
2015-12-29 12:24:37 +0530

    modifying route for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,6 @@ Spree::Core::Engine.routes.draw do
     end
   end
 
-  resources :blog_entries
-  get '/:blog_id/:slug' => 'blog_entries#show', as: :blog_entry_permalink
-
-#  get '/blog/tag/:tag' => 'blog_entries#tag', :as => :blog_tag
-#  get '/blog/category/:category' => 'blog_entries#category', :as => :blog_category
-#  get '/blog/author/:author' => 'blog_entries#author', as: :blog_author
-#  get '/blog/:year(/:month)(/:day)' => 'blog_entries#archive', :as => :news_archive, 
-#    :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
-#  get '/blog/feed' => 'blog_entries#feed', :as => :blog_feed, :format => :rss
-#  get '/blog' => 'blog_entries#index', :as => :blog
+  get '/:permalink' => 'blog_entries#show', as: :blog_entry_permalink
 end
 
