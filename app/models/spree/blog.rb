b6ba4861aa377e555c8625c941cc142d7b7301ec
Meha Desai <meha@urbanladder.com>
2015-12-30 16:43:38 +0530

    updated code for blog and blog_entry models

---
+++ app/models/spree/blog.rb
@@ -0,0 +1,26 @@
+class Spree::Blog < ActiveRecord::Base
+  has_many :blog_entries, dependent: :destroy
+
+  acts_as_paranoid
+
+  before_create :set_slug
+  after_create :set_css_path
+
+  validates_presence_of :name
+  # can add a validation to allow enabling only if AT LEAST ONE blog entry is present
+
+  SLUG_LENGTH = 20
+  RECENT_POSTS = 10
+
+  def set_slug
+    self.slug = name.to_url[0..(SLUG_LENGTH - 1)] if slug.blank?
+  end
+
+  def set_css_path
+    self.css_path = 'spree/frontend/' + slug
+  end
+
+  def recent
+    blog_entries.order('published_at DESC').limit(RECENT_POSTS)
+  end
+end 
