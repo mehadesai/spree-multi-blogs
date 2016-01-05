35d19df7177b04e4d3303d85e988ca4f7f07b8ff
Meha Desai <meha@urbanladder.com>
2016-01-05 13:04:52 +0530

    update seed script

--- db/seeds/spree_multi_blogs_seed.rb
+++ db/seeds/spree_multi_blogs_seed.rb
@@ -1,4 +1,4 @@
-require 'ffaker'
+require 'ffaiker'
 
 namespace :db do
   namespace :seed do
@@ -11,6 +11,7 @@ namespace :db do
 
         puts "Start seeding..."
         BLOGS = 5
+        TRIES = 5
         BLOG_POSTS = (50..100).to_a
         DAYS = (1..365).to_a
         users = Spree::User.includes(:spree_roles).where(spree_roles: { name: 'admin' }).limit(10)
@@ -25,11 +26,33 @@ namespace :db do
             puts "\tPost #{blog_entry_index}"
             be = Spree::BlogEntry.new
             be.blog_id = b.id
-            be.title = Faker::Movie.title
-            be.published_at = Time.now - DAYS.sample.days
+            
+            published_at = Time.now - DAYS.sample.days
+            title_words = (1..6).to_a.sample
+            title = Faker::BaconIpsum.words(title_words).shuffle.join(' ').titleize
+            permalink = [b.slug, published_at.strftime('%Y'), published_at.strftime('%m'), title.to_url].join('/')
+
+            TRIES.times do |try|
+              blog_entry_exists = Spree::BlogEntry.find_by(permalink: permalink)
+              if blog_entry_exists.present?
+                puts "\tTry #{try}...#{permalink} - #{blog_entry_exists.present?}"
+                published_at = Time.now - DAYS.sample.days
+                permalink = [b.slug, published_at.strftime('%Y'), published_at.strftime('%m'), title.to_url].join('/')
+                title_words = (1..6).to_a.sample
+                title = Faker::BaconIpsum.words(title_words).shuffle.join(' ').titleize
+              else
+                next
+              end
+            end
+
+            be.title = title
+            be.published_at = published_at
             be.summary = Faker::BaconIpsum.sentences
             be.author_id = users.sample.id
-            be.body = Faker::HTMLIpsum
+
+            body_arr = ['a', 'body', 'fancy_string', 'p', 'ul_long']
+            be.body = body_arr.sample(3).map { |b| Faker::HTMLIpsum.send(b.to_sym) }.join('')
+
             be.save!
           end
         end
