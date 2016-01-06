b11d275de1781e6501774d133d04ebb304bc1ab4
Meha Desai <meha@urbanladder.com>
2016-01-06 17:51:53 +0530

    removed unused code

--- app/controllers/spree/blog_entries_controller.rb
+++ app/controllers/spree/blog_entries_controller.rb
@@ -1,13 +1,8 @@
 class Spree::BlogEntriesController < Spree::StoreController
   helper 'spree/blog_entries' 
-
-  before_filter :init_pagination, only: :index
+ 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  
-  def index
-    @blog_entries = Spree::BlogEntry.visible.page(@pagination_page).per(@pagination_per_page)
-  end
-  
+
   def show
     permalink = [params[:slug], params[:year], params[:month], params[:permalink]].join('/')
     if try_spree_current_user.try(:has_spree_role?, 'admin')
@@ -20,11 +15,4 @@ class Spree::BlogEntriesController < Spree::StoreController
     end
     @title = @blog_entry.title
   end
-
-  private
-
-    def init_pagination
-      @pagination_page = params[:page].to_i > 0 ? params[:page].to_i : 1
-      @pagination_per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 10
-    end
 end
