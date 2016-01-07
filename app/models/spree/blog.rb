bfcd828cdb79c3beb5a553163322ff04d5cd7d7d
Meha Desai <meha@urbanladder.com>
2016-01-07 20:06:32 +0530

    allow models to inherit from core spree core - for validation errors etc

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -1,32 +1,35 @@
-class Spree::Blog < ActiveRecord::Base
-  has_many :blog_entries, dependent: :destroy
+module Spree
+  class Blog < Spree::Base
+    has_many :blog_entries, dependent: :destroy
 
-  acts_as_paranoid
+    acts_as_paranoid
 
-  before_create :set_slug
-  after_create :set_css_path
+    before_create :set_slug
+    after_create :set_css_path
 
-  validates_presence_of :name
-  validates_uniqueness_of :name
-  # can add a validation to allow enabling only if AT LEAST ONE blog entry is present
+    validates_presence_of :name
+    validates_uniqueness_of :name
+    validates_uniqueness_of :slug
+    # can add a validation to allow enabling only if AT LEAST ONE blog entry is present
 
-  SLUG_LENGTH = 20
-  RECENT_POSTS = 10
-  scope :enabled, -> { where(enabled: true) }
+    SLUG_LENGTH = 20
+    RECENT_POSTS = 10
+    scope :enabled, -> { where(enabled: true) }
 
-  def set_slug
-    self.slug = name.to_url[0..(SLUG_LENGTH - 1)] if slug.blank?
-  end
+    def set_slug
+      self.slug = name.to_url[0..(SLUG_LENGTH - 1)] if slug.blank?
+    end
 
-  def set_css_path
-    self.css_path = 'spree/frontend/' + slug
-  end
+    def set_css_path
+      self.css_path = 'spree/frontend/' + slug
+    end
 
-  def recent
-    blog_entries.visible.published.limit(RECENT_POSTS)
-  end
+    def recent
+      blog_entries.visible.published.limit(RECENT_POSTS)
+    end
 
-  def self.retrieve_slugs
-    Spree::Blog.enabled.collect(&:slug)
+    def self.retrieve_slugs
+      Spree::Blog.enabled.collect(&:slug)
+    end
   end
 end 
--- app/models/spree/blog_entry.rb
+++ app/models/spree/blog_entry.rb
@@ -1,50 +1,40 @@
-class Spree::BlogEntry < ActiveRecord::Base
-  belongs_to :blog
+module Spree
+  class BlogEntry < Spree::Base
+    belongs_to :blog
 
-  before_save :set_published_at
-  before_save :set_permalink
+    before_save :set_published_at
+    before_save :set_permalink
 
-  validates_presence_of :title
-  validates_presence_of :body
-  validates_uniqueness_of :blog_id, scope: :permalink
+    validates_presence_of :title
+    validates_presence_of :body
+    validates_uniqueness_of :blog_id, scope: :permalink
 
-  default_scope { order('published_at DESC') }
-  scope :visible, -> { where(visible: true) }
-  scope :published, -> { where('published_at <= ?', Time.now) }
+    default_scope { order('published_at DESC') }
+    scope :visible, -> { where(visible: true) }
+    scope :published, -> { where('published_at <= ?', Time.now) }
 
-  if Spree.user_class
-    belongs_to :author, :class_name => Spree.user_class.to_s
-  else
-    belongs_to :author
-  end
-
-  has_one :blog_entry_image, :as => :viewable, :dependent => :destroy, :class_name => 'Spree::BlogEntryImage'
-  accepts_nested_attributes_for :blog_entry_image, :reject_if => :all_blank
-
-  SUMMARY_CHARS = 200
-
-  def entry_summary(chars=SUMMARY_CHARS)
-    if summary.blank?
-      "#{body[0...chars]}..."
+    if Spree.user_class
+      belongs_to :author, :class_name => Spree.user_class.to_s
     else
-      summary
+      belongs_to :author
     end
-  end
 
-  private
-  def set_permalink
-    base_permalink = blog.slug + published_at.strftime('/%Y/%m/')
-    permalink_part = if permalink.blank?
-                       title
-                     elsif permalink.start_with?(base_permalink)
-                       permalink.split('/').last
-                     else
-                       permalink
-                     end
-    self.permalink = base_permalink + permalink_part.to_url
-  end
+    private
+  
+      def set_permalink
+        base_permalink = blog.slug + published_at.strftime('/%Y/%m/')
+        permalink_part = if permalink.blank?
+                           title
+                         elsif permalink.start_with?(base_permalink)
+                           permalink.split('/').last
+                         else
+                           permalink
+                         end
+        self.permalink = base_permalink + permalink_part.to_url
+      end
 
-  def set_published_at
-    self.published_at = Time.now if published_at.blank?
+      def set_published_at
+        self.published_at = Time.now if published_at.blank?
+      end
   end
 end 
