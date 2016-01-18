bacc4cd175f94f8b57beb90e913311d303909db9
Meha Desai <meha@urbanladder.com>
2016-01-18 17:37:36 +0530

    update method to fetch top/recent tags

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -12,7 +12,7 @@ module Spree
     # can add a validation to allow enabling only if AT LEAST ONE blog entry is present
 
     SLUG_LENGTH = 20
-    RECENT_POSTS = 10
+    RECENT_POSTS = 5
     scope :enabled, -> { where(enabled: true) }
 
     def set_css_path
@@ -25,15 +25,12 @@ module Spree
 
     def tags_by_type(count, type)
       tags_data = blog_entries.includes(:tags)
-      tags_data = if type == 'recent'
-                    tags_data.order('spree_blog_entries_tags.updated_at DESC')
-                  elsif type == 'top'
-                    tags_data
-                  end
+      tags_data = tags_data.order('spree_blog_entries_tags.updated_at DESC') if type == 'recent'
+
       tags_hash = {}
-      # tags_hash = tags_hash.each { |be| be.tags.inject(Hash.new(1)) { |h, t| h[t.name] += 1; h } }
       tags_data.each { |be| be.tags.each { |t| tags_hash[t.name] = tags_hash[t.name].present? ? tags_hash[t.name] += 1 : 1 } }
-      tags_hash.first(count).to_h
+      tags_hash = tags_hash.sort_by {|k, v| v }.reverse if type == 'top'
+      tags_hash = tags_hash.first(count).to_h
     end
 
     def self.retrieve_slugs
