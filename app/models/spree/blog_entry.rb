a368c68df9085db0c162bd7652682858ae7526ec
Meha Desai <meha@urbanladder.com>
2016-01-28 14:40:02 +0530

    fill in default meta_description and meta_title for blog posts

--- app/models/spree/blog_entry.rb
+++ app/models/spree/blog_entry.rb
@@ -7,6 +7,9 @@ module Spree
     before_save :set_published_at
     before_save :set_permalink
 
+    after_save :set_default_meta_title, if: Proc.new{ meta_title.blank? }
+    after_save :set_default_meta_description, if: Proc.new{ meta_description.blank? }
+
     validates_presence_of :title
     validates_presence_of :body
     validates_uniqueness_of :blog_id, scope: :permalink
@@ -14,6 +17,8 @@ module Spree
     default_scope { order('published_at DESC') }
     scope :visible, -> { where(visible: true) }
     scope :published, -> { where('published_at <= ?', Time.now) }
+
+    META_DESCRIPTION_LIMIT = (155..160).to_a
 
     if Spree.user_class
       belongs_to :author, :class_name => Spree.user_class.to_s
@@ -51,6 +56,34 @@ module Spree
 
       def set_published_at
         self.published_at = Time.now if published_at.blank?
+      end
+
+      def set_default_meta_title
+        self.update_attributes(meta_title: self.title)
+      end
+
+      def set_default_meta_description
+        meta_description = if summary.blank?
+                             fetch_default_meta_description
+                           else
+                             summary
+                           end
+        self.update_attributes(meta_description: meta_description) unless meta_description.blank?
+      end
+
+      def fetch_default_meta_description
+        description = ActionView::Base.full_sanitizer.sanitize(body).gsub( / *\n+/, "" )
+        if META_DESCRIPTION_LIMIT.include?(description.length)
+          meta_description = description
+        else
+          short_description = description.split('.').first
+          meta_description = if description.to_s.length > META_DESCRIPTION_LIMIT.max
+                               short_description[0..(META_DESCRIPTION_LIMIT.min - 1)]
+                             else
+                               short_description
+                             end
+        end
+        meta_description
       end
   end
 end 
